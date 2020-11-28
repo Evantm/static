294cd25de1a967267f887835e2f38a11d1eb28e4
Evan  ™️ <etmackay@gmail.com>
2020-11-28 14:59:38 -0800

    Added actual code

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+fetch('http://evanmackay.ca/static/time.json').then((response) => {
+        return response.json()
+    }).then((data) => {
+        console.log(data) $("#time").html("The time is: " + data.utc_datetime);
+    }).catch((err) => { // Really? This code broke? Well okay then }) 
