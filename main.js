ae71107ea2a4f0b70ef44f00a771a3450166ce6e
Evan  ™️ <etmackay@gmail.com>
2020-11-29 16:22:07 -0800

    Fixed incorrect id refrence on new cases

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 fetch('http://evanmackay.ca/static/time.json').then((response) => {
         return response.json()
     }).then((data) => {
-        console.log(data) 
         $("#time").html("The time is: " + data.utc_datetime);
     }).catch((err) => { }) 
 
@@ -9,6 +8,5 @@ fetch('http://evanmackay.ca/static/time.json').then((response) => {
 fetch('http://evanmackay.ca/static/bc.json').then((response) => {
         return response.json()
     }).then((data) => {
-        console.log(data) 
-        $("active").html("New Cases on" + data[data.date -1].numtoday + ": " + data[data.length -1].numtoday);
+        $("#active").html("New Cases on" + data[data.date -1].numtoday + ": " + data[data.length -1].numtoday);
     }).catch((err) => {  }) 
